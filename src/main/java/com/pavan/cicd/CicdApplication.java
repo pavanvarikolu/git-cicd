ff937176aeac8b00b316b6baf614b4499d371aeb
pavanvarikolu <pavanvarikolu@gmail.com>
2023-03-18 20:32:56 +0530

    first commit

---
+++ src/main/java/com/pavan/cicd/CicdApplication.java
@@ -0,0 +1,22 @@
+package com.pavan.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class CicdApplication {
+
+	@RequestMapping("/")
+	public String welocome(){
+		return "<h1>welcome..</h1>";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
